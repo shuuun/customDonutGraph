6c24da799fee0ec4519ed85e7a032acdbd812feb
shuuun <shun.arupakaman12@gmail.com>
2020-04-15 15:44:57 +0900

    fix

--- lib/home.dart
+++ lib/home.dart
@@ -13,28 +13,26 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   AnimationController _controller;
   final percentage = ValueNotifier<double>(0.0);
-  double randomPercentage = 0.0;
+  final randomPercentage = ValueNotifier<double>(0.0);
 
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: Duration(seconds: 1))
+    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 1000))
       ..addListener(() { 
-        percentage.value = lerpDouble(0, randomPercentage, _controller.value);
+        percentage.value = lerpDouble(percentage.value, randomPercentage.value, _controller.value);
       });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Donut Graph')),
+      appBar: AppBar(title: Text('')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            randomPercentage = Random().nextInt(100).toDouble();
-            _controller.forward(from: 0.0);
-          });
+          randomPercentage.value = Random().nextInt(100).toDouble();
+          _controller.forward(from: 0.0);
         },
         child: Icon(Icons.refresh),
       ),
